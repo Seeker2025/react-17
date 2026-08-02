bb5769660c60a52d791c34e192d761ab0844e503
Igor Yurovsky <85400723+Seeker2025@users.noreply.github.com>
2026-08-02 12:59:42 +0300

    Search
    added Search

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,29 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from "react";
+
+import { Search } from './Search/Search';
+import { ContentInfo } from "./ContentInfo/ContentInfo";
+
+export class App extends Component{
+
+    state ={
+      searchText: '',
+    };
+
+    handleSearch = (searchText) =>{
+        this.setState({ searchText })
+    }
+
+  render(){
+
+          return (
+                <div>
+                
+                  <Search/>
+                  
+                  <ContentInfo searchText={this.state.searchText}/>
+
+
+                </div>
+          );      
+}
+}
---
+++ src/components/Search/Search.jsx
@@ -0,0 +1,42 @@
+import { Component } from "react";
+import css from './Search.module.css';
+
+export class Search extends Component{
+
+    state ={
+        value: '',
+    }
+
+    handleChange = (e) => {
+        this.setState({ value:e.target.value })
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(this.state);
+    }
+
+    render(){
+        return (
+            <>
+        <form 
+        className = 'd-flex mt-2'  
+        role = 'search'
+        onSubmit = {this.handleSubmit}
+        >
+                    <input
+                        className='form-control me-2'
+                        type = 'search'
+                        placeholder='Search'
+                        aria-label = 'Search'
+                        onChange ={this.handleChange}
+                    />
+
+            <button className='btn btn-outline-success' type='submit'>
+                Search
+            </button>        
+        </form>
+            </>
+        )
+    }
+}
